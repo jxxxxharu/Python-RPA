6f729163ddac2a6911925ccd4ce7f2c08e2b68e8
JoEun Yang <jxxxxharu@gmail.com>
2022-07-25 21:57:18 +0900

    무한 스크롤, 필요한 정보 스크래핑

--- selenium_basic.py
+++ selenium_basic.py
@@ -30,3 +30,30 @@ search.click()
 """검색창 입력"""
 search.send_keys('바디로션')
 search.send_keys(Keys.ENTER)
+
+
+"""무한 스크롤"""
+before_h = browser.execute_script('return window.scrollY')  # 스크롤 전 높이
+
+while True:
+    browser.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.END)  # 맨 아래로 스크롤을 내린다.
+    time.sleep(1)  # 스크롤 사이 페이지 로딩 시간
+    
+    after_h = browser.execute_script('return window.scrollY')  # 스크롤 후 높이
+    if after_h == before_h: 
+        break
+    before_h = after_h
+    
+    
+"""상품정보 div"""
+items = browser.find_elements(By.CSS_SELECTOR, '.basicList_info_area__17Xyo')
+
+for item in items:
+    name = item.find_element(By.CSS_SELECTOR, '.basicList_title__3P9Q7').text
+    try:
+        price = item.find_element(By.CSS_SELECTOR, '.price_num__2WUXn').text
+    except:
+        price = '판매중단'
+    link = item.find_element(By.CSS_SELECTOR, '.basicList_title__3P9Q7 > a').get_attribute('href')
+    
+    print(name, price, link)
